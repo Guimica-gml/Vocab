4a003856f51730b841875175536e227c90cd2906
Nicholas Almeida Albuquerque <e.nicholas.albuquerque@mpt.mp.br>
2024-08-09 15:39:42 -0300

    Fix build.bat cause I am dumb

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 if not exist build (mkdir build)
 
-cl.exe /std:clatest /c /Fo:build/vocab.obj main.c /I include/raylib/src && link.exe /out:build/vocab.exe build/vocab.obj lib/raylib.lib
+cl.exe /std:clatest /c /Fo:build/vocab.obj main.c /I include/raylib-5.0/src && link.exe /out:build/vocab.exe build/vocab.obj lib/raylib.lib
