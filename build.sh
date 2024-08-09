97b7596689a166d922c7e8b4dd9bd59b890613de
Guimica-gml <nicholasalmeida567@gmail.com>
2024-08-09 14:46:52 -0300

    Add build to windows (kind of)
    There is still a lot to do, like adding a way to build raylib, and
    also update the linux build step

--- build.sh
+++ build.sh
@@ -4,10 +4,11 @@ set -e
 CFLAGS="-Wall -Wextra -pedantic -ggdb -std=c11"
 CLIBS="-lraylib"
 
-gcc $CFLAGS -o vocab main.c $CLIBS
+mkdir -p ./build
+gcc $CFLAGS -o ./build/vocab main.c $CLIBS
 
 if [ "$1" = "run" ]
 then
     shift
-    ./vocab "$@"
+    ./build/vocab "$@"
 fi
